01c1ef401464d46ed53c508544f1f8e7742f9bc5
John Ouellet <john@thinktandem.io>
2020-06-06 13:15:13 -0400

    Cleanup code some

--- acf-hidden.php
+++ acf-hidden.php
@@ -4,46 +4,23 @@
 Plugin Name: Advanced Custom Fields: Hidden Field
 Plugin URI: http://folbert.com
 Description: A hidden field for Advanced Custom Fields
-Version: 1.5.1
-Author: folbert
+Version: 1.5.2
+Author: folbert, John Ouellet
 Author URI: http://folbert.com
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
+/**
+ * Add Language.
+ */
+load_plugin_textdomain( 'acf-hidden', false, dirname( plugin_basename(__FILE__) ) . '/lang/' );
 
-
-
-// 1. set text domain
-// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
-load_plugin_textdomain( 'acf-hidden', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
-
-
-
-
-// 2. Include field type for ACF5
-// $version = 5 and can be ignored until ACF6 exists
+/**
+ * Add field.
+ */
 function include_field_types_hidden( $version ) {
-	
 	include_once('acf-hidden-v5.php');
-	
 }
+add_action('acf/include_field_types', 'include_field_types_hidden');
 
-add_action('acf/include_field_types', 'include_field_types_hidden');	
-
-
-
-
-// 3. Include field type for ACF4
-function register_fields_hidden() {
-	
-	include_once('acf-hidden-v4.php');
-	
-}
-
-add_action('acf/register_fields', 'register_fields_hidden');	
-
-
-
-	
-?>
